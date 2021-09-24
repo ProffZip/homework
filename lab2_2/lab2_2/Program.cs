479e9b197116bdfa552d1547ce504961d0a66d4a
Ivan Matviienko <proffzip2@gmail.com>
2021-09-24 15:13:12 +0300

    lab 2.2

---
+++ lab2_2/lab2_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side1:");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter side2:");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(side1, side2);
+            rectangle.AreaCalculator();
+            rectangle.PerimeterCalculator();
+            Console.WriteLine("Area ="+ rectangle.Area);
+            Console.WriteLine("Perimeter =" + rectangle.Perimeter);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab2_2/lab2_2/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Rectangle
+{
+    private double _side1, _side2;
+
+	public Rectangle(double Side1, double Side2)
+    {
+        _side1 = Side1;
+        _side2 = Side2;
+    }
+    public double Area
+    {
+        get
+        {
+            return AreaCalculator();
+        }
+    }
+    public double Perimeter
+    {
+        get
+        {
+            return PerimeterCalculator();
+        }
+    }
+
+    public double AreaCalculator()
+    {
+        double Area = _side1 * _side2;
+        return Area;
+    }
+    public double PerimeterCalculator()
+    {
+        double Perimeter = (_side1 + _side2)*2;
+        return Perimeter;
+    }
+    
+
+}
