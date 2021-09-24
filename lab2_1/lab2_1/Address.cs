1628139007f97b8d93107add8517ab298c63de76
Ivan Matviienko <proffzip2@gmail.com>
2021-09-24 14:11:34 +0300

    lab 2.1

---
+++ lab2_1/lab2_1/Address.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+public class Address
+{
+    private int _index;
+
+    public int Index
+    {
+        get
+        {
+            return _index;
+        }
+        set
+        {
+            this._index = value;
+        }
+    }
+
+    private int _apartment;
+
+    public int Apartment
+    {
+        get
+        {
+            return _apartment;
+        }
+        set
+        {
+            this._apartment = value;
+        }
+    }
+    private int _house;
+
+    public int House
+    {
+        get
+        {
+            return _house;
+        }
+        set
+        {
+            this._house = value;
+        }
+    }
+    private string _country;
+
+    public string Country
+    {
+        get
+        {
+            return _country;
+        }
+        set
+        {
+            this._country = value;
+        }
+    }
+    private string _city;
+
+    public string City
+    {
+        get
+        {
+            return _city;
+        }
+        set
+        {
+            this._city = value;
+        }
+    }
+    private string _street;
+
+    public string Street
+    {
+        get
+        {
+            return _street;
+        }
+        set
+        {
+            this._street = value;
+        }
+    }
+    public void WriteAddress()
+    {
+        Console.WriteLine($"Country: {_country}");
+        Console.WriteLine($"Index: {_index}");
+        Console.WriteLine($"City: {_city}");
+        Console.WriteLine($"Street: {_street}");
+        Console.WriteLine($"House: {_house}");
+        Console.WriteLine($"Apartment: {_apartment}");
+    }
+
+}
---
+++ lab2_1/lab2_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.Index = 02217;
+            address.House = 32;
+            address.Apartment = 69;
+            address.Country = "Ukraine";
+            address.City = "Kyiv";
+            address.Street = "Kyoto";
+            address.WriteAddress();
+            Console.ReadKey();
+
+        }
+
+       
+    }
+}
