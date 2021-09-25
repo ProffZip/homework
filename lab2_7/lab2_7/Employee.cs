456764ccd0a7989bbedce6c4f6bda46ed94cc6fd
Ivan Matviienko <proffzip2@gmail.com>
2021-09-25 18:57:34 +0300

    lab2.7, sheeesh only last one left

---
+++ lab2_7/lab2_7/Employee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+public class Employee
+{
+	private string _lastname;
+	private string _name;
+	private double _exp;
+	private string _pos;
+	public double Exp
+    {
+        get
+        {
+            return _exp;
+        }
+        set
+        {
+            this._exp = value;
+        }
+    }
+    public string Pos
+    {
+        get
+        {
+            return _pos;
+        }
+        set
+        {
+            this._pos = value;
+        }
+    }
+    public Employee(string lastname, string name)
+    {
+		_lastname = lastname;
+		_name = name;
+    }
+
+    
+    public double Endpay(double basepay, Employee employee)
+    {
+        double endpay = basepay * (1 + (0.05 * employee.Exp));
+        return endpay;
+    }
+
+    public double Pay(Employee employee)
+    {
+        double basepay;
+        double endpay = 0;
+        switch (employee.Pos)
+        {
+            case "worker":
+                basepay = 10000;
+                endpay = employee.Endpay(basepay, employee);
+                break;
+            case "manager":
+                basepay = 20000;
+                endpay = employee.Endpay(basepay, employee);
+                break;
+            case "director":
+                basepay = 30000;
+                endpay = employee.Endpay(basepay, employee);
+                break;
+        }
+        return endpay;
+        
+    }
+    public double Tax(Employee employee)
+    {
+        double tax = employee.Pay(employee) * 0.195;
+        return tax;
+    }
+    public void PayData(Employee employee)
+    {
+        double pay = employee.Pay(employee);
+        double tax = employee.Tax(employee);
+        Console.WriteLine("The current pay is "+pay+" minus the tax of "+tax);
+    }
+    public void EmployeeInfo(Employee employee)
+    {
+        Console.WriteLine("Info: "+employee._name+" "+employee._lastname+ " " + employee.Pos);
+        employee.PayData(employee);
+    }
+}
---
+++ lab2_7/lab2_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your surname, than name:");
+            string surname =Console.ReadLine();
+            string name = Console.ReadLine();
+            Employee employee = new Employee(surname, name);
+            Console.WriteLine("Please enter your position(worker, manager, director):");
+            employee.Pos = Console.ReadLine();
+            Console.WriteLine("Please enter your experience in full years:");
+            employee.Exp = Convert.ToDouble( Console.ReadLine());
+            employee.EmployeeInfo(employee);
+            Console.ReadLine();
+
+        }
+    }
+}
