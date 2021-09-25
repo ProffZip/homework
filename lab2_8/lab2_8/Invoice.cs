eeda1b08b9120449c0a3a4d99eca802d9b2396f6
Ivan Matviienko <proffzip2@gmail.com>
2021-09-25 22:10:37 +0300

    lab2.8 урааа аоаоаоаоаоао

---
+++ lab2_8/lab2_8/Invoice.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+public class Invoice
+{
+	private int _account;
+	private string _customer;
+	private string _provider;
+	private string _article;
+	private int _quantity;
+    public int Account
+    {
+        get
+        {
+            return _account;
+        }
+    }
+    public string Customer
+    {
+        get
+        {
+            return _customer;
+        }
+    }
+    public string Provider
+    {
+        get
+        {
+            return _provider;
+        }
+    }
+    public string Article
+    {
+        get
+        {
+            return _article;
+        }
+        set
+        {
+            this._article = value;
+        }
+    }
+    public int Quantity
+    {
+        get
+        {
+            return _quantity;
+        }
+        set
+        {
+            this._quantity = value;
+        }
+
+    }
+    public Invoice(int account, string customer, string provider)
+    {
+        _account = account;
+        _customer = customer;
+        _provider = provider;
+    }
+    public double Cost(Invoice invoice)
+    {
+        double basecost;
+        double cost = 0;
+        switch (invoice._article)
+        {
+            case "car":
+                basecost = 300000;
+                cost = invoice._quantity * basecost;
+                break;
+            case "phone":
+                basecost = 20000;
+                cost = invoice._quantity * basecost;
+                break;
+            case "bike":
+                basecost = 50000;
+                cost = invoice._quantity * basecost;
+                break;
+        }
+        return cost;
+
+    }
+    public double CostNDS(Invoice invoice)
+    {
+        double costnds = invoice.Cost(invoice)*1.2;
+
+        return costnds;
+    }
+    public void InvoiceInfo(Invoice invoice)
+    {
+        Console.WriteLine("Order info:\nAccount:"+invoice.Account+"\nOrdered by: "+invoice.Customer+"\nGoods provided by: "+invoice.Provider);
+        Console.WriteLine("You've ordered "+invoice.Quantity+" "+invoice.Article+"s");
+        Console.WriteLine("You'll be charged "+Cost(invoice)+" and including VAT(20%) - "+invoice.CostNDS(invoice));
+    }
+}
---
+++ lab2_8/lab2_8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your order info");
+            Console.WriteLine("Account id:");
+            int account = Convert.ToInt32( Console.ReadLine());
+
+            Console.WriteLine("Customer name:");
+            string customer = Console.ReadLine();
+
+            Console.WriteLine("Povider of goods:");
+            string provider = Console.ReadLine();
+
+            Invoice invoice = new Invoice(account, customer, provider);
+
+            Console.WriteLine("Please select the goods:\nCar, phone, bike");
+            invoice.Article = Console.ReadLine();
+            Console.WriteLine("Please enter the quantity:");
+            invoice.Quantity = Convert.ToInt32( Console.ReadLine());
+            invoice.InvoiceInfo(invoice);
+            Console.ReadLine();
+        }
+    }
+}
