44dfa64ac3aec2bb008ed3f703424cec36878f0d
Ivan Matviienko <proffzip2@gmail.com>
2021-09-25 16:50:22 +0300

    lab2.6

---
+++ lab2_6/lab2_6/Converter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+public class Converter
+{
+
+    private double _usd;
+    private double _eur;
+    private double _rub;
+    public double USD
+    {
+        get
+        {
+            return _usd;
+        }
+    }
+    public double EUR
+    {
+        get
+        {
+            return _eur;
+        }
+    }
+    public double RUB
+    {
+        get
+        {
+            return _rub;
+        }
+    }
+    
+    
+    public Converter(double usd, double eur, double rub )
+    {
+        _usd = usd;
+        _eur = eur;
+        _rub = rub;
+       
+    }
+    public void ConvUAH(int a, int b, double sum, Converter converter)
+    {
+        switch (a)
+        {
+            case 1:// convert uah INTO smth
+                switch (b)//decide into what
+                {
+                    case 1:
+                        double UahUsd = sum / converter.USD;
+                        Console.WriteLine(sum + "UAH is"+UahUsd+"USD");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        double UahEur = sum / converter.EUR;
+                        Console.WriteLine(sum + "UAH is"+ UahEur+"EUR" );
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        double UahRub = sum / converter.RUB;
+                        Console.WriteLine(sum + "UAH is" + UahRub+"RUB"  );
+                        Console.ReadLine();
+                        break;
+                }
+                break;
+            case 2:// convert smth INTO uah
+                switch (b)//decide what
+                {
+                    case 1:
+                        double UsdUah = sum * converter.USD;
+                        Console.WriteLine(sum + "USD is"+UsdUah+"UAH");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        double EurUah = sum * converter.EUR;
+                        Console.WriteLine(sum + "EUR is"+EurUah+"UAH");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        double RubUah = sum * converter.USD;
+                        Console.WriteLine(sum + "RUB is"+RubUah+"UAH");
+                        Console.ReadLine();
+                        break;
+                }
+                break;
+
+        }
+
+    }
+
+}
---
+++ lab2_6/lab2_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the exchange rate of currencies to UAH");
+
+            Console.WriteLine("USD:");
+            double usd = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("EUR:");
+            double eur = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("RUB:");
+            double rub = Convert.ToDouble(Console.ReadLine());
+
+            Converter converter = new Converter(usd, eur, rub);
+
+            Console.WriteLine("Do you want to convert UAH into smth(1) or smth into UAH(2)?");
+            int a = Convert.ToInt32( Console.ReadLine());
+
+            Console.WriteLine("Which currency would you like to use?");
+
+            Console.WriteLine("USD(1), EUR(2), RUB(3)");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the sum to convert");
+            double sum = Convert.ToDouble(Console.ReadLine());
+
+            converter.ConvUAH(a, b, sum, converter);
+
+        }
+    }
+}
