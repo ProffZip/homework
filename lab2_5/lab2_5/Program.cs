f238f88b494006c9f7cbfe17d9c32fae35e0b4f2
Ivan Matviienko <proffzip2@gmail.com>
2021-09-25 15:51:23 +0300

    lab2.5

---
+++ lab2_5/lab2_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your info here:\nLogin:");
+            string login = Console.ReadLine();
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            User user = new User(login, name, lastname, age);
+            user.UserInfo(user);
+
+        }
+    }
+}
---
+++ lab2_5/lab2_5/User.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class User
+{
+    private string _login;
+    private string _name;
+    private string _lastname;
+    private int _age;
+    private string _date;
+    public string Login
+    {
+        get
+        {
+            return _login;
+        }
+        set
+        {
+            this._login = value;
+        }
+    }
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            this._name = value;
+        }
+    }
+    public string LastName
+    {
+        get
+        {
+            return _lastname;
+        }
+        set
+        {
+            this._lastname = value;
+        }
+    }
+    public int Age
+    {
+        get
+        {
+            return _age;
+        }
+        set
+        {
+            this._age = value;
+        }
+    }
+    public string Date
+    {
+        get
+        {
+            return _date;
+        }
+    }
+
+    public User(string login, string name, string lastname, int age)
+    {
+        _login = login;
+        _name = name;
+        _lastname = lastname;
+        _age = age;
+        _date = DateTime.Today.ToString("D");
+    }
+
+    public void UserInfo(User user)
+    {
+        Console.WriteLine("Current info is:\nLogin:"+user.Login+"\nName:"+user.Name+ "\nLast Name:" + user.LastName+ "\nAge:" + user.Age+ "\nDate of creation:" + user.Date);
+        Console.ReadLine();
+        
+    }
+}
