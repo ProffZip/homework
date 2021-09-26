d680085346d2fe8634241ccf71b63144b3926a67
Ivan Matviienko <proffzip2@gmail.com>
2021-09-26 18:52:41 +0300

    lab3 0_0

---
+++ lab3/lab3/Calculator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+public class Calculator
+{
+	private string _line;
+    private double[] _num;
+    private char _action;
+
+    public string Line
+    {
+        get
+        {
+            return _line;
+        }
+    }
+    public char Action
+    {
+        get
+        {
+            return _action;
+        }
+    }
+
+    public double[] Nums
+    {
+        get
+        {
+            return _num;
+        }
+        
+    }
+
+    public Calculator(string line)
+    {
+        _line = line;
+        double[] Nums = new double[2];
+        _num = Nums;
+    }
+    public void Calculating(Calculator calculator)
+    {
+        NumDivide(calculator);
+        Count(calculator);
+    }
+    public void NumDivide(Calculator calculator)
+    {
+        string line = calculator.Line.Trim(' ', '=', '/', '|', '?', '!', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z');
+        string line1 = line.Replace('.', ',');
+        string[] num = line1.Split('+', '*', '-');
+        calculator.Nums[0] = Convert.ToDouble(num[0]);
+        calculator.Nums[1] = Convert.ToDouble(num[1]);
+
+    }
+    public void Count(Calculator calculator)
+    {
+        if (calculator.Line.Contains("+"))
+        {
+            Console.WriteLine("Your sum is "+Plus(calculator));
+        }
+        else
+        {
+            if (calculator.Line.Contains("-"))
+            {
+                Console.WriteLine("Your difference is " + Minus(calculator));
+            }
+            else
+            {
+                if (calculator.Line.Contains("*"))
+                {
+                    Console.WriteLine("Your product is " + Mult(calculator));
+                }
+                else
+                {
+                    Console.WriteLine("You've entered the wrong operator, please restart");
+                    
+                }
+
+            }
+        }
+        
+    }
+
+    public double Plus(Calculator calculator)
+    {
+        double sum = calculator.Nums[0] + calculator.Nums[1];
+        return sum;
+    }
+    public double Minus(Calculator calculator)
+    {
+        double min = calculator.Nums[0] - calculator.Nums[1];
+        return min;
+    }
+    public double Mult(Calculator calculator)
+    {
+        double mult = calculator.Nums[0] * calculator.Nums[1];
+        return mult;
+    }
+
+
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a simple calculator \nwhere you can add(+), subtract(-) and multiply(*) two numbers\n\nYou can enter your expression in one line or like this: \nNum1-enter-Operator-enter-Num2");
+        a:
+            try
+            {
+                Console.WriteLine("\n\nPlease enter your expression:");
+                string line = Console.ReadLine();
+                try
+                {
+                    Calculator calculator = new Calculator(line);
+                    calculator.Calculating(calculator);
+                }
+                catch(IndexOutOfRangeException)
+                {
+               
+                    double line1 = Convert.ToDouble(line);
+                    char action = Convert.ToChar(Console.ReadLine());
+                    double line2 = Convert.ToDouble(Console.ReadLine());
+                    switch (action)
+                    {
+                        case '+':
+                            Console.WriteLine("Your sum is " + (line1 + line2));
+                            break;
+                        case '-':
+                            Console.WriteLine("Your difference is " + (line1 - line2));
+                            break;
+                        case '*':
+                            Console.WriteLine("Your product is " + (line1 * line2));
+                            break;
+                        default:
+                            Console.WriteLine("You've entered the wrong operator");
+                            goto a;
+                    }
+
+                }
+            
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("This is not a correct expression");
+                goto a;
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
