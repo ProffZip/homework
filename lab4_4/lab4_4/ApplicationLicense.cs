c5d2e77a616316e5890375b02fef7fdc9883e545
Ivan Matviienko <proffzip2@gmail.com>
2021-10-20 14:18:14 +0300

    lab4.4

---
+++ lab4_4/lab4_4/ApplicationLicense.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public class ApplicationLicense
+{
+	private string licensecode;
+	public string LicenseCode
+    {
+        get
+        {
+			return licensecode;
+        }
+        set
+        {
+			licensecode = value;
+        }
+    }
+	public void LicenseCheck(ApplicationLicense license)
+    {
+		if (String.IsNullOrWhiteSpace(license.LicenseCode))
+        {
+			Console.WriteLine("You have not entered the license code");
+			AllowCommon(license);
+		}
+        else
+        {
+			
+			if (AllowTrial(license)|| AllowPro(license))
+            {
+
+            }
+            else
+            {
+				Console.WriteLine("You have entered the wrong license code");
+				AllowCommon(license);
+			}
+        }
+		
+
+	}
+	public void AllowCommon(ApplicationLicense license)
+	{
+		Console.WriteLine("Now you have a free version");
+	}
+	public bool AllowTrial(ApplicationLicense license) 
+	{
+		if (license.LicenseCode.Equals("00000000"))
+		{
+			Console.WriteLine("Now you have a trial version");
+			return true;
+		}
+        else
+		{
+			return false;
+		}
+	}
+	public bool AllowPro(ApplicationLicense license)
+	{
+		if (license.LicenseCode.Equals("11111111"))
+        {
+			Console.WriteLine("Now you have a full version");
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
---
+++ lab4_4/lab4_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your license code");
+            string code = Console.ReadLine();
+            ApplicationLicense license = new ApplicationLicense();
+            license.LicenseCode = code;
+            license.LicenseCheck(license);
+            Console.ReadLine();
+
+        }
+    }
+}
