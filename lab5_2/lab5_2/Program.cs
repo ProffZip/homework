319c61bcd108ca5376f7a88436689df877b9ff7b
Ivan Matviienko <proffzip2@gmail.com>
2021-11-11 16:18:09 +0200

    lab5.2

---
+++ lab5_2/lab5_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1, для расчёта обычного конуса, и 2 - для усеченного");
+            Cone.input = Convert.ToInt32(Console.ReadLine());
+            if (Cone.input == 1)
+            {
+                Cone.Input();
+                Cone cone = new Cone();
+            }
+            else if (Cone.input == 2)
+            {
+                Conoid.Input1();
+                Conoid conoid = new Conoid();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
