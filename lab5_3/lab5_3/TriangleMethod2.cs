129a4af59d03a90285634f832f89c4d5c62f65a8
Ivan Matviienko <proffzip2@gmail.com>
2021-11-11 16:29:06 +0200

    lab5.3

---
+++ lab5_3/lab5_3/TriangleMethod2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class TriangleMethod2 : UPMatrix
+    {
+        public TriangleMethod2()
+        {
+            TriangleMethod1.TriangleCount();
+        }
+    }
+
---
+++ lab5_3/lab5_3/UPMatrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class UPMatrix : Matrix
+    {
+        public UPMatrix()
+        {
+            Inputter();
+        }
+        public void Inputter()
+        {
+            Console.WriteLine("Выберите способ просчёта определителя матрицы: ");
+            Console.WriteLine("Введите '1', если методом 'треугольника', введите '2' - если методом Саррюса");
+            input = Convert.ToInt32(Console.ReadLine());
+        }
+    }
